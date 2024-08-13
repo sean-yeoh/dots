a15478d4e6a1c60c76c8f4de0d5a14f5f09613c0
Sean Yeoh <dev@seanysx.com>
2024-08-13 15:23:32 +0800

    Flameshot workaround

--- install.sh
+++ install.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 chmod 600 ~/.ssh/id_rsa
 
 sudo pacman -S --needed base-devel
@@ -9,15 +11,29 @@ sudo pacman -S --needed base-devel
 # cd ~/dots
 
 mkdir -p ~/.local/share/icons
+mkdir -p ~/.local/share/fonts
 
-cp -r ./assets/Reversal-blue-dark ~./.local/share/icons
-cp -r ./assets/Bibata-Modern-Ice ~./.local/share/icons
+cp -r ./assets/Reversal-blue-dark ~/.local/share/icons/
+cp -r ./assets/Bibata-Modern-Ice ~/.local/share/icons/
+cp -r ./assets/fonts/* ~/.local/share/fonts/
+fc-cache
 
 gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
 gsettings set org.gnome.desktop.interface icon-theme 'Reversal-blue-dark'
 gsettings set org.gnome.desktop.interface cursor-theme 'Bibata-Modern-Ice'
 
-yay -S -needed google-chrome slack-desktop code spotify-launcher flameshot \
+yay -S --needed google-chrome slack-desktop code spotify-launcher flameshot \
   gnome-extensions-cli kooha hivemind-bin libmagick6 graphicsmagick pass \
-  kitty
+  kitty zsh
 
+if [[ $SHELL != *"zsh"* ]]; then
+  chsh -s $(which zsh)
+fi
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+git clone --depth=1 https://github.com/zsh-users/zsh-autosuggestions.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone --depth=1 https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone --depth=1 https://gitee.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/powerlevel10k
+
+sudo cp ./scripts/flameshot-workaround /usr/bin/
