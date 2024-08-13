ed1888f374e52753124627803bdf0142b7a5bd5e
Sean Yeoh <dev@seanysx.com>
2024-08-13 10:32:41 +0800

    Add theme

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # dots
+
+### References
+- https://github.com/Fausto-Korpsvart/Kanagawa-GKT-Theme
+- https://github.com/Keyitdev/sddm-astronaut-theme
+- https://github.com/hyprwm/Hyprland/discussions/5867#discussioncomment-9313777
+- https://github.com/LOSEARDES77/Bibata-Cursor-hyprcursor/
--- install.sh
+++ install.sh
@@ -1,3 +1,9 @@
-yay -S --needed gtk-engine-murrine gvfs thunar-archive-plugin \
-  thunar-media-tags-plugin thunar-shares-plugin thunar-volman \
-  tumbler ffmpegthumbnailer 
+yay -S --needed gtk-engine-murrine
+
+mkdir -p ~/.themes
+cp -r ./assets/Kanagawa-BL-LB ~/.themes
+mkdir -p ~/.config/gtk-4.0
+cp -r ./assets/Kanagawa-BL-LB/gtk-4.0/assets ~/.config/gtk-4.0/
+cp ./assets/Kanagawa-BL-LB/gtk-4.0/gtk-dark.css ~/.config/gtk-4.0/
+cp ./assets/Kanagawa-BL-LB/gtk-4.0/gtk.css ~/.config/gtk-4.0/
+
