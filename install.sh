846d7b10311d780c0c8f5891f05504f47cd2e475
Sean Yeoh <dev@seanysx.com>
2024-08-13 14:27:33 +0800

    Add icons

--- install.sh
+++ install.sh
@@ -1,17 +1,20 @@
 chmod 600 ~/.ssh/id_rsa
 
-sudo pacman -S --needed base-devel gtk-engine-murrine
-
-mkdir -p ~/.themes
-cp -r ./assets/Kanagawa-BL-LB ~/.themes
-mkdir -p ~/.config/gtk-4.0
-cp -r ./assets/Kanagawa-BL-LB/gtk-4.0/assets ~/.config/gtk-4.0/
-cp ./assets/Kanagawa-BL-LB/gtk-4.0/gtk-dark.css ~/.config/gtk-4.0/
-cp ./assets/Kanagawa-BL-LB/gtk-4.0/gtk.css ~/.config/gtk-4.0/
-
-gsettings set org.gnome.desktop.interface gtk-theme 'Kanagawa-BL-LB'
-gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
+sudo pacman -S --needed base-devel
 
 git clone https://aur.archlinux.org/yay-bin.git
 cd yay-bin
 makepkg -si
+
+mkdir -p ~/.local/share/icons
+
+cp -r ./assets/Reversal-blue-dark ~./.local/share/icons
+cp -r ./assets/Bibata-Modern-Ice ~./.local/share/icons
+
+gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
+gsettings set org.gnome.desktop.interface icon-theme 'Reversal-blue-dark'
+gsettings set org.gnome.desktop.interface cursor-theme 'Bibata-Modern-Ice'
+
+yay -S -needed google-chrome slack-desktop code spotify-launcher flameshot \
+  gnome-extensions-cli kooha hivemind-bin libmagick6 graphicsmagick pass
+
