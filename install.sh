5e424d658b3803b58aa6c7fdf73fb1503bbf6077
Sean Yeoh <dev@seanysx.com>
2024-08-13 10:37:25 +0800

    Add yay

--- install.sh
+++ install.sh
@@ -1,4 +1,6 @@
-yay -S --needed gtk-engine-murrine
+chmod 600 ~/.ssh/id_rsa
+
+sudo pacman -S --needed base-devel gtk-engine-murrine
 
 mkdir -p ~/.themes
 cp -r ./assets/Kanagawa-BL-LB ~/.themes
@@ -9,3 +11,7 @@ cp ./assets/Kanagawa-BL-LB/gtk-4.0/gtk.css ~/.config/gtk-4.0/
 
 gsettings set org.gnome.desktop.interface gtk-theme 'Kanagawa-BL-LB'
 gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
+
+git clone https://aur.archlinux.org/yay-bin.git
+cd yay-bin
+makepkg -si
