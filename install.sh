41f69ce7f1de8b759000e16a8ad6bb15399f5a9e
Sean Yeoh <dev@seanysx.com>
2024-08-13 10:34:23 +0800

    Add gsettings

--- install.sh
+++ install.sh
@@ -7,3 +7,5 @@ cp -r ./assets/Kanagawa-BL-LB/gtk-4.0/assets ~/.config/gtk-4.0/
 cp ./assets/Kanagawa-BL-LB/gtk-4.0/gtk-dark.css ~/.config/gtk-4.0/
 cp ./assets/Kanagawa-BL-LB/gtk-4.0/gtk.css ~/.config/gtk-4.0/
 
+gsettings set org.gnome.desktop.interface gtk-theme 'Kanagawa-BL-LB'
+gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
