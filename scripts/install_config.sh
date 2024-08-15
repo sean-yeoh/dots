580b16002d56b08df5dedae2c993a68d50055c98
Sean Yeoh <dev@seanysx.com>
2024-08-15 18:12:03 +0800

    Fix path

--- scripts/install_config.sh
+++ scripts/install_config.sh
@@ -1,7 +1,7 @@
 mkdir -p ~/.config/kitty
 
-cp ../config/kitty/* ~/.config/kitty/
-cp ../config/.gitconfig ~/.gitconfig
-cp ../config/.p10k.zsh ~/.p10.zsh
-cp ../config/.zshrc ~/.zshrc
-cp ../config/.tool-versions ~/.tool-versions
+cp ./config/kitty/* ~/.config/kitty/
+cp ./config/.gitconfig ~/.gitconfig
+cp ./config/.p10k.zsh ~/.p10.zsh
+cp ./config/.zshrc ~/.zshrc
+cp ./config/.tool-versions ~/.tool-versions
--- scripts/install_theme.sh
+++ scripts/install_theme.sh
@@ -1,9 +1,9 @@
 mkdir -p ~/.local/share/icons
 mkdir -p ~/.local/share/fonts
 
-cp -r ../assets/Reversal-blue-dark ~/.local/share/icons/
-cp -r ../assets/Bibata-Modern-Ice ~/.local/share/icons/
-cp ../assets/fonts/* ~/.local/share/fonts/
+cp -r ./assets/Reversal-blue-dark ~/.local/share/icons/
+cp -r ./assets/Bibata-Modern-Ice ~/.local/share/icons/
+cp ./assets/fonts/* ~/.local/share/fonts/
 
 gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
 gsettings set org.gnome.desktop.interface icon-theme 'Reversal-blue-dark'
