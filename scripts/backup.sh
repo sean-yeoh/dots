5f1a2de5abb3b45f6406c33a0336afad9ce0229d
Sean Yeoh <dev@seanysx.com>
2024-08-16 12:27:36 +0800

    Backup configs

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -1,5 +1,8 @@
-cp ~/.p10k.zsh ../config/
-cp ~/.zshrc ../config/
-cp ~/.gitconfig ../config/
-cp ~/.tool-versions ../config/
-cp -r ~/.config/kitty/ ../config/
+cp ~/.p10k.zsh ./config/
+cp ~/.zshrc ./config/
+cp ~/.gitconfig ./config/
+cp ~/.tool-versions ./config/
+cp -r ~/.config/kitty/ ./config/
+cp ~/.config/code-flags.conf ./config/
+cp ~/.config/chrome-flags.conf ./config/
+cp ~/.config/zoomus.conf ./config/
