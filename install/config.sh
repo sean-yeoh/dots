42822d316dadb0971bd64b2b731a9cce37326f62
Sean Yeoh <dev@seanysx.com>
2024-08-16 11:14:38 +0800

    Copy flags

--- install/config.sh
+++ install/config.sh
@@ -2,6 +2,8 @@ mkdir -p ~/.config/kitty
 
 cp -r ./config/kitty ~/.config/
 cp ./config/zoomus.conf ~/.config/
+cp ./config/code-flags.conf ~/.config/
+cp ./config/chrome-flags.conf ~/.config/
 
 cp ./config/.gitconfig ~/
 cp ./config/.p10k.zsh ~/
