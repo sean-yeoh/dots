3130b7dac84081442cfd56a2139cedf037068df4
sean-yeoh <dev@seanysx.com>
2024-08-18 09:10:55 +0800

    Update config.sh

--- install/config.sh
+++ install/config.sh
@@ -1,9 +1,9 @@
 mkdir -p ~/.config/kitty
 
 cp -r ./config/kitty ~/.config/
-cp ./config/zoomus.conf ~/.config/
-cp ./config/code-flags.conf ~/.config/
-cp ./config/chrome-flags.conf ~/.config/
+#cp ./config/zoomus.conf ~/.config/
+#cp ./config/code-flags.conf ~/.config/
+#cp ./config/chrome-flags.conf ~/.config/
 
 cp ./config/.gitconfig ~/
 cp ./config/.p10k.zsh ~/
