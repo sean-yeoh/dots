3e71b0b62ec7b79a6a6349f4c18ec922b85fdc10
Sean Yeoh <dev@seanysx.com>
2024-08-19 13:43:31 +0800

    Backup kitty

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -3,6 +3,6 @@ cp ~/.zshrc ./config/
 cp ~/.gitconfig ./config/
 cp ~/.tool-versions ./config/
 cp -r ~/.config/kitty/ ./config/
-cp ~/.config/code-flags.conf ./config/
-cp ~/.config/chrome-flags.conf ./config/
+# cp ~/.config/code-flags.conf ./config/
+# cp ~/.config/chrome-flags.conf ./config/
 cp ~/.config/zoomus.conf ./config/
