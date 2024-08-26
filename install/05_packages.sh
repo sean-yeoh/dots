97879a96935b69c4244f1ea15a4a66c0905e6827
Sean Yeoh <dev@seanysx.com>
2024-08-26 22:52:32 +0800

    Update package.sh

---
+++ install/05_packages.sh
@@ -0,0 +1,2 @@
+yay -S --needed --noconfirm google-chrome slack-desktop spotify-launcher flameshot \
+  kooha vlc shotwell zoom man ttf-fira-sans visual-studio-code-bin 
--- install/packages.sh
+++
@@ -1,4 +0,0 @@
-yay -S --needed --noconfirm google-chrome slack-desktop spotify-launcher flameshot \
-  gnome-extensions-cli kooha vlc shotwell zoom man ttf-fira-sans gdm-settings
-
-# visual-studio-code-bin 
