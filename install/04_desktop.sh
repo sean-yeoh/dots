da96337f803fdd24e10959ced8e4ee0de21405fe
Sean Yeoh <dev@seanysx.com>
2024-08-27 10:11:20 +0800

    Update desktop.sh

---
+++ install/04_desktop.sh
@@ -0,0 +1,4 @@
+sudo yay -S --needed --noconfirm hyprland brightnessctl gtk-engine-murrine
+
+mkdir -p ~/.local/share/themes
+cp -r ./assets/Kanagawa ~/.local/share/themes
--- install/desktop.sh
+++
@@ -1,3 +0,0 @@
-sudo pacman -S --needed --noconfirm gnome gnome-extra gnome-shell
-sudo systemctl enable gdm.service
-sudo systemctl enable bluetooth.service
