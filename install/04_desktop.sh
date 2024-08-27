2dcea28ab01e7504408a1570f497ed5eeba7e7c1
Sean Yeoh <dev@seanysx.com>
2024-08-27 11:14:20 +0800

    Update desktop.sh

--- install/04_desktop.sh
+++ install/04_desktop.sh
@@ -3,8 +3,10 @@ sudo yay -S --needed --noconfirm hyprland brightnessctl gtk-engine-murrine
 mkdir -p ~/.local/share/themes
 mkdir -p ~/.local/share/icons
 cp -r ./assets/Material-Black-Mango ~/.local/share/themes
-cp -r ./assets/Tela ~/.local/share/icons
+cp -r ./assets/Reversal-dark ~/.local/share/icons
 
 xdg-user-dirs-update
 
 yay -S --needed --noconfirm nemo nemo-fileroller nemo-preview xdg-user-dirs
+
+gtk-update-icon-cache ~/.local/share/icons/Reversal-dark
